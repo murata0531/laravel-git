888f06bd10709ffcbb3f7c478ddec3c0a00a5fd9
murata0531 <coless115@gmail.com>
2022-08-14 14:52:05 +0900

    tasksテーブルを追加

---
+++ src/app/database/migrations/2022_08_14_051634_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('タスク名');
+            $table->text('description')->comment('詳細');
+            $table->unsignedBigInteger('status_id')->default(1)->comment('タスクの状態');
+            $table->timestamps();
+
+            $table->foreign('status_id')->references('id')->on('statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
